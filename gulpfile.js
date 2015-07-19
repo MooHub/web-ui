29370668c9c2ebf0388b2f135cdaec7c8ec62313
Salvatore Laisa <moebiusmania@gmail.com>
2015-07-20 00:13:37 +0200

    added SASS for styles, updated Gulp actions

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var minifyHTML = require('gulp-minify-html');
+var sass = require('gulp-sass');
 
-gulp.task('default', ['minify-html']);
+gulp.task('default', ['minify-html','sass']);
 
 gulp.task('minify-html', function() {
   return gulp.src('./src/index.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest(''));
 });
+
+gulp.task('sass', function () {
+  gulp.src('./src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(''));
+});
+
+gulp.watch(['./src/index.html','./src/style.scss'],['default']);
